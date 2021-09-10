e0f0773dad640e955692465a594553290bd9a437
HitachiinKaoru <ishyrohitachiin@gmail.com>
2021-09-10 02:21:50 -0300

    Ajustes

--- RealTurismo/Menu.xaml.cs
+++ RealTurismo/Menu.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using MahApps.Metro.Controls;
-using Oracle.ManagedDataAccess.Client;
+﻿using MahApps.Metro.Controls;
 
 namespace RealTurismo
 {
@@ -26,9 +12,5 @@ namespace RealTurismo
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
