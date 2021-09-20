76d5418296dd7be60a25c1164a9abbe2be9ecf46
MarceloFernandez01 <MarceloAFJ99@gmail.com>
2021-09-19 23:06:25 -0300

    CRUD Agregar depto terminado
    Falta:
    Corregir bugs
    implementar reglas de negocios
    resto del crud departamento

--- RealTurismo/MantDepartamento.xaml.cs
+++ RealTurismo/MantDepartamento.xaml.cs
@@ -54,8 +54,6 @@ namespace RealTurismo
 
         private void cbbRegion_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            cbbProvincia.DataContext = null;
-            cbbProvincia.Items.Clear();
             //Crear conexion
             OracleConnection conexionOracle = new OracleConnection(cadenaConexionOracle);
             // Conecta
@@ -68,38 +66,131 @@ namespace RealTurismo
                         "inner join region r " +
                         "on r.id_region = p.id_region " +
                         "where r.descripcion = '" + region + "'";
-            MessageBox.Show(query2);
             OracleCommand Comando2 = new OracleCommand(query2, conexionOracle);
             OracleDataReader lector2 = Comando2.ExecuteReader();
             while (lector2.Read())
             {
                 cbbProvincia.Items.Add(lector2["Descripcion"].ToString());
             }
+            
         }
         
         private void cbbProvincia_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            /*cbbComuna.DataContext = null;
+            cbbComuna.DataContext = null;
             cbbComuna.Items.Clear();
 
             //Crear conexion
             OracleConnection conexionOracle = new OracleConnection(cadenaConexionOracle);
             // Conecta
             conexionOracle.Open();
-
             string provincia = cbbProvincia.SelectedItem.ToString();
             //cargar comunas
             string query3 = $"select c.descripcion from comuna c "+
                             "inner join provincia p "+
                             "on p.id_provincia = c.iid_provincia "+
                             "where p.descripcion = '"+ provincia + "'";
-            MessageBox.Show(query3);
             OracleCommand Comando3 = new OracleCommand(query3, conexionOracle);
             OracleDataReader lector3 = Comando3.ExecuteReader();
             while (lector3.Read())
             {
                 cbbComuna.Items.Add(lector3["Descripcion"].ToString());
-            }*/
+            }
+        }
+
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            //obtener datos
+            string nombre = txtNombre.Text;
+            string direccion = txtDireccion.Text;
+            string costo = txtCosto.Text;
+            string piso = txtPiso.Text;
+            string cable = "0";
+            string internet = "0";
+            string calefaccion = "0";
+            string amoblado = "0";
+            string aire = "0";
+            string balcon = "0";
+            string disponible = "0";
+            if (cbCable.IsChecked == true)
+            {
+                cable = "1";
+            }
+            if (cbInternet.IsChecked == true)
+            {
+                internet = "1";
+            }
+            if (cbCalefaccion.IsChecked == true)
+            {
+                calefaccion = "1";
+            }
+            if (cbAmoblado.IsChecked == true)
+            {
+                amoblado = "1";
+            }
+            if (cbAire.IsChecked == true)
+            {
+                aire = "1";
+            }
+            if (cbBalcon.IsChecked == true)
+            {
+                balcon = "1";
+            }
+            string nrohabitaciones = txtNHabitaciones.Text;
+            string nrobaios = txtNBanios.Text;
+            string nroPersonas = txtNPersonas.Text;
+            if (cbDisponible.IsChecked == true)
+            {
+                disponible = "1";
+            }
+
+            string comuna = cbbComuna.SelectedItem.ToString();
+
+            //Crear conexion
+            OracleConnection conexionOracle = new OracleConnection(cadenaConexionOracle);
+            // Conecta
+            conexionOracle.Open();
+
+            string buscarIdComuna = "select id_comuna from comuna "+
+                "where descripcion = '"+ comuna +"'";
+
+            OracleCommand Comando = new OracleCommand(buscarIdComuna, conexionOracle);
+
+            OracleDataReader buscarID = Comando.ExecuteReader();
+
+            if(buscarID.Read())
+            {
+                string id = buscarID.GetString(0);
+
+                string añadirDepto = "INSERT INTO DEPARTAMENTO VALUES(" +
+                "incremento_id_departamento.NextVal,'" + nombre + "','" + direccion + "'," + piso + "," + costo +
+                "," + cable + "," + internet + "," + calefaccion + "," + amoblado + "," + aire + "," + balcon + "," + nrohabitaciones +
+                "," + nrobaios + "," + nroPersonas + "," + disponible + "," + id+")";
+
+                MessageBox.Show(añadirDepto);
+
+                OracleCommand insertar = new OracleCommand(añadirDepto, conexionOracle);
+                OracleDataReader CrearDepartamento = insertar.ExecuteReader();
+                if (CrearDepartamento.Read())
+                {
+                    MessageBox.Show("Se añadio departamento");
+                }
+
+                //pordia añadir un commit
+                /*
+                OracleCommand insertar = new OracleCommand(añadirDepto, conexionOracle);
+                OracleTransaction transaccion;
+                transaccion = conexionOracle.BeginTransaction(IsolationLevel.ReadCommitted);
+                insertar.CommandText = añadirDepto;
+                insertar.ExecuteNonQuery();
+                transaccion.Commit();
+                MessageBox.Show("Se inserto el departamento");*/
+                
+
+            }
+
+
+
         }
     }
 }
