b0e9d1668f3862be9a8e95b63a229e8ad410ec29
Elise Grey <elisetomoe@gmail.com>
2021-10-11 23:51:24 -0300

    Interfaces: menuEmpleado, admUsuario, ingresos, egresos, informes

---
+++ RealTurismo/MenuEmpleado.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RealTurismo
+{
+    /// <summary>
+    /// Lógica de interacción para MenuEmpleado.xaml
+    /// </summary>
+    public partial class MenuEmpleado : Window
+    {
+        public MenuEmpleado()
+        {
+            InitializeComponent();
+        }
+
+        private void btnADepartamentos_Click(object sender, RoutedEventArgs e)
+        {
+            MantDepartamento depto = new MantDepartamento();
+            depto.Show();
+        }
+
+        private void btnIngresosEmp_Click(object sender, RoutedEventArgs e)
+        {
+            VisualizarIngresos ingresos = new VisualizarIngresos();
+            ingresos.Show();
+        }
+
+        private void btnEgresosEmp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
