ca159de0f59f54cd77e2a0f0f879e340258855b8
MarceloFernandez01 <MarceloAFJ99@gmail.com>
2021-10-25 20:08:23 -0300

    Crud de departamente con procesos almacenados (listar por id, agregar y eliminar listo) pendiente modificar

--- RealTurismo/Vistas/AdministrarDepartamentos.xaml.cs
+++ RealTurismo/Vistas/AdministrarDepartamentos.xaml.cs
@@ -54,11 +54,85 @@ namespace RealTurismo.Vistas
                 Controlador.ControladorDepartamento depto = new Controlador.ControladorDepartamento();
                 dgListadoDep.ItemsSource = depto.ListarDepartamentos();
             }
+            else
+            {
+                Controlador.ControladorDepartamento depto = new Controlador.ControladorDepartamento();
+                dgListadoDep.ItemsSource = depto.ListarDeptoID(int.Parse(iddepto));
+            }
 
         }
         private void dgListadoDep_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            Modelo.Departamentos depto = (Modelo.Departamentos)dgListadoDep.SelectedItem;
+            if (dgListadoDep.SelectedItem != null)
+            {
+                txtIdDepto.Text = depto.IdDepartamento.ToString();
+                txtNombre.Text = depto.NombreDescriptivo.ToString();
+                txtDireccion.Text = depto.Direccion.ToString();
+                txtCosto.Text = depto.Costo.ToString();
+                txtPiso.Text = depto.Piso.ToString();
+                if (depto.Cable == false)
+                {
+                    cbCable.IsChecked = false;
+                }
+                else
+                {
+                    cbCable.IsChecked = true;
+                }
+                if (depto.Internet == false)
+                {
+                    cbInternet.IsChecked = false;
+                }
+                else
+                {
+                    cbInternet.IsChecked = true;
+                }
+                if (depto.Calefaccion == false)
+                {
+                    cbCalefaccion.IsChecked = false;
+                }
+                else
+                {
+                    cbCalefaccion.IsChecked = true;
+                }
+                if (depto.Amoblado == false)
+                {
+                    cbAmoblado.IsChecked = false;
+                }
+                else
+                {
+                    cbAmoblado.IsChecked = true;
+                }
+                if (depto.AireAcondiconado == false)
+                {
+                    cbAireAcondicionado.IsChecked = false;
+                }
+                else
+                {
+                    cbAireAcondicionado.IsChecked = true;
+                }
+                if (depto.Balcon == false)
+                {
+                    cbBalcon.IsChecked = false;
+                }
+                else
+                {
+                    cbBalcon.IsChecked = true;
+                }
+                txtNHabitaciones.Text = depto.NroHabitaciones.ToString();
+                txtNBanios.Text = depto.NroBanios.ToString();
+                txtNPersonas.Text = depto.CantidadPersonas.ToString();
+                if (depto.Disponible == false)
+                {
+                    cbDisponible.IsChecked = false;
+                }
+                else
+                {
+                    cbDisponible.IsChecked = true;
+                }
+                cbbRegion.SelectedIndex = 0;
 
+            }
         }
 
         private void cbbRegion_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -107,7 +181,7 @@ namespace RealTurismo.Vistas
                 //cargar comunas
                 string query3 = $"select c.descripcion from comuna c " +
                                 "inner join provincia p " +
-                                "on p.id_provincia = c.iid_provincia " +
+                                "on p.id_provincia = c.id_provincia " +
                                 "where p.descripcion = '" + provincia + "'";
                 OracleCommand Comando3 = new OracleCommand(query3, conexionOracle);
                 OracleDataReader lector3 = Comando3.ExecuteReader();
@@ -121,9 +195,161 @@ namespace RealTurismo.Vistas
             dgListadoDep.ItemsSource = depto.ListarDepartamentos();
         }
 
-        private void cbbRegion_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
-        {
 
+        private void btnAgregar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Modelo.Departamentos depto = new Modelo.Departamentos();
+                depto.NombreDescriptivo = txtNombre.Text;
+                depto.Direccion = txtDireccion.Text;
+                depto.Costo = int.Parse(txtCosto.Text);
+                depto.Piso = int.Parse(txtPiso.Text);
+                int cable = 0;
+                int internet = 0;
+                int calefaccion = 0;
+                int amoblado = 0;
+                int aire = 0;
+                int balcon = 0;
+                int disponible = 0;
+                if (cbCable.IsChecked == true)
+                {
+                    cable = 1;
+                }
+                if (cbInternet.IsChecked == true)
+                {
+                    internet = 1;
+                }
+                if (cbCalefaccion.IsChecked == true)
+                {
+                    calefaccion = 1;
+                }
+                if (cbAmoblado.IsChecked == true)
+                {
+                    amoblado = 1;
+                }
+                if (cbAireAcondicionado.IsChecked == true)
+                {
+                    aire = 1;
+                }
+                if (cbBalcon.IsChecked == true)
+                {
+                    balcon = 1;
+                }
+                depto.NroHabitaciones = int.Parse(txtNHabitaciones.Text);
+                depto.NroBanios = int.Parse(txtNBanios.Text);
+                depto.CantidadPersonas = int.Parse(txtNPersonas.Text);
+                if (cbDisponible.IsChecked == true)
+                {
+                    disponible = 1;
+                }
+                string comuna = cbbComuna.SelectedItem.ToString();
+                OracleConnection conexionOracle = new OracleConnection(cadenaConexionOracle);
+                conexionOracle.Open();
+                string buscarIdComuna = "select id_comuna from comuna " +
+                    "where descripcion = '" + comuna + "'";
+                OracleCommand Comando = new OracleCommand(buscarIdComuna, conexionOracle);
+                OracleDataReader buscarID = Comando.ExecuteReader();
+                if (buscarID.Read())
+                {
+                    int id = int.Parse(buscarID.GetString(0));
+                    Controlador.ControladorDepartamento ControlDepto = new Controlador.ControladorDepartamento();
+                    bool añadir = ControlDepto.AñadirDepto(depto.NombreDescriptivo,depto.Direccion, depto.Piso,depto.Costo,
+                        cable, internet, calefaccion, amoblado, aire, balcon, depto.NroHabitaciones,
+                        depto.NroBanios, depto.CantidadPersonas, disponible, id);
+                    if(añadir == true)
+                    {
+                        MessageBox.Show("El departamento ha sido ingresado con exito");
+                        txtIdDepto.Text = "";
+                        txtNombre.Text = "";
+                        txtDireccion.Text = "";
+                        txtCosto.Text = "";
+                        txtPiso.Text = "";
+                        cbCable.IsChecked = false;
+                        cbInternet.IsChecked = false;
+                        cbCalefaccion.IsChecked = false;
+                        cbAmoblado.IsChecked = false;
+                        cbAireAcondicionado.IsChecked = false;
+                        cbBalcon.IsChecked = false;
+                        txtNHabitaciones.Text = "";
+                        txtNBanios.Text = "";
+                        txtNPersonas.Text = "";
+                        cbDisponible.IsChecked = false;
+                        cbbComuna.DataContext = null;
+                        cbbComuna.Items.Clear();
+                        cbbProvincia.DataContext = null;
+                        cbbProvincia.Items.Clear();
+                        cbbRegion.SelectedIndex = 0;
+                        cbbProvincia.SelectedIndex = 0;
+                        cbbComuna.SelectedIndex = 0;
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se pudo ingresar el departamento");
+                    }
+                    
+                    dgListadoDep.ItemsSource = ControlDepto.ListarDepartamentos();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error : " + ex.Message);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int id = int.Parse(txtIdDepto.Text);
+                if (MessageBox.Show("¿Esta seguro que desea eliminar el siguiente departamento? : " + id, "Advertencia", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.No)
+                {
+                    MessageBox.Show("Operacion Abortada");
+                }
+                else
+                {
+                    Controlador.ControladorDepartamento ControlDepto = new Controlador.ControladorDepartamento();
+                    bool eliminar = ControlDepto.EliminarDepto(id);
+                    if (eliminar == true)
+                    {
+                        txtIdDepto.Text = "";
+                        txtNombre.Text = "";
+                        txtDireccion.Text = "";
+                        txtCosto.Text = "";
+                        txtPiso.Text = "";
+                        cbCable.IsChecked = false;
+                        cbInternet.IsChecked = false;
+                        cbCalefaccion.IsChecked = false;
+                        cbAmoblado.IsChecked = false;
+                        cbAireAcondicionado.IsChecked = false;
+                        cbBalcon.IsChecked = false;
+                        txtNHabitaciones.Text = "";
+                        txtNBanios.Text = "";
+                        txtNPersonas.Text = "";
+                        cbDisponible.IsChecked = false;
+                        cbbComuna.DataContext = null;
+                        cbbComuna.Items.Clear();
+                        cbbProvincia.DataContext = null;
+                        cbbProvincia.Items.Clear();
+                        cbbRegion.SelectedIndex = 0;
+                        cbbProvincia.SelectedIndex = 0;
+                        cbbComuna.SelectedIndex = 0;
+                        MessageBox.Show("El departamento escogido fue eliminado con exito");
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se pudo eliminar el departamento");
+                    }
+
+                    
+                    
+                    dgListadoDep.ItemsSource = ControlDepto.ListarDepartamentos();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error : " + ex.Message);
+            }
         }
     }
 }
